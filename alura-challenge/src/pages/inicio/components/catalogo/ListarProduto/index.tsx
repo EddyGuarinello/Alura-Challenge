693656b92860ac1a95e2dc6fbc8fe3bb64b48183
Eddy Guarinello <87097263+EddyGuarinello@users.noreply.github.com>
2023-03-11 19:40:26 -0300

    NO MORE HARDCODED DATA!

--- alura-challenge/src/pages/inicio/components/catalogo/ListarProduto/index.tsx
+++ alura-challenge/src/pages/inicio/components/catalogo/ListarProduto/index.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import './style.scss';
 import Lista from '../lista';
+import { v4 as uuidv4 } from 'uuid';
 import {AiOutlineArrowRight} from 'react-icons/ai'
-import listaProdutos from "../../../../../data/produtos.json"
+
+import { useState, useEffect } from 'react';
+
+const id = uuidv4;
 
 interface nomeDoProduto{
     nomeProduto : string
 }
+interface Produto {
+    NOME: string;
+    CATEGORIA: string;
+    PRECO: string;
+    IMG: string;
+  }
 
 export default function ListarProduto(props: nomeDoProduto){
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+    useEffect(() => {
+        async function obterDadosDaApi() {
+          const response = await fetch('https://api-challenge-kohl.vercel.app/produtos');
+          const data = await response.json();
+          setProdutos(data);
+        }
+        obterDadosDaApi();
+      }, []);
+
+
     const nomeDoProduto = props.nomeProduto;
     
-    let produto = listaProdutos.filter(function(elemento:any){
-        return (elemento.categoria == nomeDoProduto)
-    })
+    let produto = produtos.filter(function(elemento:any){
+        return (elemento.CATEGORIA == nomeDoProduto)
+    }) 
+    console.log(produto)
 
-  
-    // let produto = Lista(nomeDoProduto);  
-    
     return(
         <section id='sectionProduto'>
         <span className='productTitle'>
@@ -26,11 +45,11 @@ export default function ListarProduto(props: nomeDoProduto){
         <div id='produtoWrap'>
         <div className='produtosWrap'>         
             {produto.map((item, index) =>(
-                <div>
-                <img src={item.img} alt="" />
+                <div key={uuidv4()}>
+                <img src={item.IMG} alt="" />
                 <span className='produtoInfos'>
-                <h3 className='produtoNome'>{item.nome}</h3>
-                <h3 className='produtoPreço'>{item.preço}</h3>
+                <h3 className='produtoNome'>{item.NOME}</h3>
+                <h3 className='produtoPreço'>{item.PRECO}</h3>
                 <a href="">Ver produto</a>
                 </span>
                 </div>
